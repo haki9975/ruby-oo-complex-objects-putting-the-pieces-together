18dce1504f044618aca65c9966f9ff9430f7af80
haki9975 <elias.robert.hakim@gmail.com>
2021-02-11 15:24:53 -0500

    commit tree 784394f97f898d22a1ef2c0f6619502563d28d68

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
-# Make your shoe class here!
+class Shoe
+attr_accessor :author, :page_count, :genre, :color, :size, :material, :construction, :brand, :condition
+  def initialize(brand)
+    @brand = brand
+  end
+ def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+ end
+  
+end
